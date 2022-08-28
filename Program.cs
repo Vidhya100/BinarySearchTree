39f4cae09444b7b16bf1f54b182a88f7e4b6cf77
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-28 22:34:53 +0530

    BST created

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BinarySearchtree
+{
+    class Program
+    {
+        public static void Main(string[] arg)
+        {
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            Console.WriteLine("Inorder Traversal : ");
+            binarySearch.Display();
+            //Console.ReadLine();
+
+        }
+    }
+}
